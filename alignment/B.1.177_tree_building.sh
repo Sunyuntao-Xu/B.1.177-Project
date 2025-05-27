7ea02cf9e411b74e483fcc115bdc74cfadb7f4e0
Sunyuntao-Xu <sx120@ic.ac.uk>
2025-05-27 18:13:29 +0100

    Create B.1.177_tree_building.sh

---
+++ alignment/B.1.177_tree_building.sh
@@ -0,0 +1,9 @@
+& "C:\iqtree-2.4.0-Windows\bin\iqtree2.exe" `
+-s "E:\key_site_seqkit_filtered_B.1.177.countrydate_filtered.fasta" `
+-m GTR+F+G4 `
+-bb 500 `
+--bnni `
+-nt AUTO `
+-mem 0.95 `
+-safe `
+-pre B117_mainTree
