f3364d51ab8787cca9bafba46699999a035aff9d
Sunyuntao Xu <sx120@ic.ac.uk>
2025-09-02 17:45:49 +0800

    Update B.1.177_identical_sequence_removal.sh

--- alignment/B.1.177_identical_sequence_removal.sh
+++ alignment/B.1.177_identical_sequence_removal.sh
@@ -9,18 +9,25 @@ $dupFa  = "$outDir\B117.duplicates.fa.gz"            # removed duplicate records
 $dupMap = "$outDir\B117.identical.detail.txt"        # duplicate_id<TAB>kept_id mapping
 $log    = "$outDir\B117.sequence_dedup.log.txt"
 
-
+# Input stats
 seqkit stats $in | Tee-Object -FilePath $log
-"Seq count (before): " + ((Get-Content $in | Select-String '^>').Count) | Tee-Object -FilePath $log -Append
+"Seq count (before): " + ((Get-Content $in | Select-String '^>').Count) |
+    Tee-Object -FilePath $log -Append
 
-
-# -s : dedupe by sequence (not name)
-# -D : write mapping duplicate_id<TAB>kept_id
-# -d : save removed duplicates as FASTA (gz)
+# Remove identical sequences by bases
+# -s : dedupe by sequence content
 # -j : threads
-seqkit rmdup -s -j 8 -D $dupMap -d $dupFa $in > $outSeq
+# -D : duplicate->kept mapping
+# -d : duplicate sequences FASTA (gz)
+# seqkit seq -w 0 : disable line wrapping
+seqkit rmdup -s -j 8 -D $dupMap -d $dupFa $in |
+    seqkit seq -w 0 > $outSeq
 
-"Seq count (after):  "  + ((Get-Content $outSeq | Select-String '^>').Count) | Tee-Object -FilePath $log -Append
-"Duplicates mapped:  "  + ((Get-Content $dupMap).Count)                    | Tee-Object -FilePath $log -Append
-"`nFirst few mappings (duplicate_id -> kept_id):"                           | Tee-Object -FilePath $log -Append
+# Output stats
+"Seq count (after):  "  + ((Get-Content $outSeq | Select-String '^>').Count) |
+    Tee-Object -FilePath $log -Append
+"Duplicates mapped:  "  + ((Get-Content $dupMap).Count) |
+    Tee-Object -FilePath $log -Append
+"`nFirst few mappings (duplicate_id -> kept_id):" |
+    Tee-Object -FilePath $log -Append
 Get-Content $dupMap -TotalCount 10 | Tee-Object -FilePath $log -Append
